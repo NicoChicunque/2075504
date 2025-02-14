6ecc9605c72dddd9a1ccb17935f377ff8714e4a3
Nicolas Chicunque <kalvinorochi@gmail.com>
2025-02-13 20:04:12 -0500

    Improved AddressablesManager.cs

--- Assets/Scripts/AddressablesManager.cs
+++ Assets/Scripts/AddressablesManager.cs
@@ -1,116 +1,60 @@
-using System;
-using System.Collections;
-using System.Data;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class CCDAddressablesManager : MonoBehaviour
 {
-    // Lista de paths de Addressables en CCD a descargar
-    [SerializeField] string[] ccdAddressablePaths;
     [SerializeField] Button downloadAddressablesButton, cleanCacheButton;
-
-    AsyncOperationHandle<Texture2D> bundleOperation;
+    [SerializeField] string catalogPath = "CCDBuildData/9a7816b4-166e-4c80-9f6c-3ef6aecebe92/e940f260-680b-42b8-81ae-f5108073674a/latest/catalog_1.0.json";
+    [SerializeField] string[] addressableKeys;
+    [SerializeField] Image _image;
 
     void Start()
     {
-        downloadAddressablesButton.onClick.AddListener(RequestBundle);
-        cleanCacheButton.onClick.AddListener(CleanCache);
+        downloadAddressablesButton.onClick.AddListener(DownloadAddressables);
+        cleanCacheButton.onClick.AddListener(CleanBundleCache);        
     }
 
-    //void DownloadAddressables()
-    //{
-    //    // Inicializar Addressables y comenzar la descarga en segundo plano
-    //    StartCoroutine(InitializeAndDownloadAddressables());
-    //}
+    void DownloadAddressables()=>Addressables.InitializeAsync().Completed += OnAddressablesInitialized;
 
-    void RequestBundle() { 
-
-        bundleOperation = Addressables.LoadAssetAsync<Texture2D>(ccdAddressablePaths[0]);
-        bundleOperation.Completed += (operation) =>
-        {
-            if (operation.Status.Equals(AsyncOperationStatus.Succeeded))
-            {
-                Debug.Log("Asset bundle loaded successfully.");
-                return;
-            }
-            Debug.LogError("Sprite load failed. Using default sprite.");
-        };
-    }
-
-    void OnDestroy()
+    void OnAddressablesInitialized(AsyncOperationHandle<IResourceLocator> initHandle)
     {
-        if (bundleOperation.IsValid())
+        Debug.Log("OnAddressablesInitialized!");
+        Addressables.LoadContentCatalogAsync(catalogPath).Completed += OnCatalogLoaded;
+        Addressables.Release(initHandle);
+    }
+
+    void OnCatalogLoaded(AsyncOperationHandle<IResourceLocator> catalogHandle)
+    {
+        Debug.Log("OnCatalogLoaded!");
+        Addressables.LoadAssetAsync<Texture2D>(addressableKeys[0]).Completed += OnTextureLoaded;
+        Addressables.Release(catalogHandle);
+    }
+
+    void OnTextureLoaded(AsyncOperationHandle<Texture2D> textureHandle)
+    {
+        Debug.Log("OnTextureLoaded!");
+        Texture2D texture = textureHandle.Result;
+        Sprite newSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),  Vector2.zero,  100f );
+        _image.sprite = newSprite;
+        //Addressables.Release(textureHandle);
+    }
+
+    void CleanBundleCache() => Addressables.CleanBundleCache().Completed += OnCleanBundleCache;
+
+    void OnCleanBundleCache(AsyncOperationHandle<bool> cleanBundleCachehandle)
+    {
+        if (cleanBundleCachehandle.Status == AsyncOperationStatus.Succeeded)
         {
-            Addressables.Release(bundleOperation);
-            Debug.Log("Successfully released atlasOperation.");
+            Debug.Log("Successfully cleaned Addressables cache.");
         }
-    }
-
-    //private IEnumerator InitializeAndDownloadAddressables()
-    //{
-    //    // Inicializar Addressables
-    //    AsyncOperationHandle initHandle = Addressables.InitializeAsync();
-    //    yield return initHandle;
-
-    //    if (initHandle.Status == AsyncOperationStatus.Succeeded)
-    //    {
-    //        Debug.Log("Addressables initialized successfully.");
-
-    //        // Descargar cada Addressable en segundo plano usando su path
-    //        foreach (string path in ccdAddressablePaths)
-    //        {
-    //            StartCoroutine(DownloadAddressable(path));
-    //        }
-    //    }
-    //    else
-    //    {
-    //        Debug.LogError("Failed to initialize Addressables.");
-    //    }
-
-    //    // Liberar el handle de inicialización
-    //    Addressables.Release(initHandle);
-    //}
-
-    //private IEnumerator DownloadAddressable(string path)
-    //{
-    //    // Descargar el Addressable desde CCD usando su path
-    //    AsyncOperationHandle downloadHandle = Addressables.DownloadDependenciesAsync(path);
-    //    yield return downloadHandle;
-
-    //    if (downloadHandle.Status == AsyncOperationStatus.Succeeded)
-    //    {
-    //        Debug.Log($"Successfully downloaded Addressable from CCD: {path}");
-    //    }
-    //    else
-    //    {
-    //        Debug.LogError($"Failed to download Addressable from CCD: {path}");
-    //    }
-
-    //    // Liberar el handle para evitar fugas de memoria
-    //    Addressables.Release(downloadHandle);
-    //}
-
-    public void CleanCache()
-    {
-        // Limpiar la caché de Addressables
-        AsyncOperationHandle cleanHandle = Addressables.CleanBundleCache();
-        cleanHandle.Completed += handle =>
+        else
         {
-            if (handle.Status == AsyncOperationStatus.Succeeded)
-            {
-                Debug.Log("Successfully cleaned Addressables cache.");
-            }
-            else
-            {
-                Debug.LogError("Failed to clean Addressables cache.");
-            }
+            Debug.LogError("Failed to clean Addressables cache.");
+        }
 
-            // Liberar el handle de limpieza
-            Addressables.Release(cleanHandle);
-        };
+        Addressables.Release(cleanBundleCachehandle);
     }
 }
