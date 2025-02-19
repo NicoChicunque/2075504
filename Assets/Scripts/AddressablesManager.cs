d2828d74454287460d9b67c36d0b8c65b5a72fbe
Nicolas Chicunque <kalvinorochi@gmail.com>
2025-02-19 03:29:17 -0500

    Improved AddressablesManager.cs, added Assets/AddressableAssetsData/CacheInitializationSettings.asset, modified:   Assets/AddressableAssetsData/AddressableAssetSettings.asset to set CacheInitializationSettings.asset, saved Assets/ProfilerDataTests/ProfilerDataMaximumCacheSize100.data and Assets/ProfilerDataTests/ProfilerDataMaximumCacheSize4096.data

--- Assets/Scripts/AddressablesManager.cs
+++ Assets/Scripts/AddressablesManager.cs
@@ -1,44 +1,81 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class CCDAddressablesManager : MonoBehaviour
 {
-    [SerializeField] Button downloadAddressablesButton, cleanCacheButton;
-    [SerializeField] string catalogPath = "CCDBuildData/9a7816b4-166e-4c80-9f6c-3ef6aecebe92/e940f260-680b-42b8-81ae-f5108073674a/latest/catalog_1.0.json";
-    [SerializeField] string[] addressableKeys;
-    [SerializeField] Image _image;
+    [SerializeField] Button _downloadAddressablesButton, _cleanCacheButton;
+    [SerializeField] string _remoteCatalogPath = "https://8f75c7dd-ca77-4182-89c8-2c4ee0db415e.client-api.unity3dusercontent.com/client_api/v1/environments/testing/buckets/e940f260-680b-42b8-81ae-f5108073674a/release_by_badge/latest/entry_by_path/content/?path=catalog_1.0.json";
+    [SerializeField] List<string> addressableKeys;
+    [SerializeField] Image _templateImage;
+
+#if UNITY_EDITOR
+    [SerializeField] List<Object> assets;
+
+    [ContextMenu("SetKeys")]
+    void SetKeys()
+    {
+        addressableKeys.Clear();
+
+        foreach (Object asset in assets)
+        {
+            string key = AssetDatabase.GetAssetPath(asset);
+            addressableKeys.Add(key);
+        }
+    }
+#endif
 
     void Start()
     {
-        downloadAddressablesButton.onClick.AddListener(DownloadAddressables);
-        cleanCacheButton.onClick.AddListener(CleanBundleCache);        
+        _downloadAddressablesButton.onClick.AddListener(DownloadAddressables);
+        _cleanCacheButton.onClick.AddListener(CleanBundleCache);
     }
 
-    void DownloadAddressables()=>Addressables.InitializeAsync().Completed += OnAddressablesInitialized;
+    void DownloadAddressables()
+    {
+        Addressables.InitializeAsync().Completed += OnAddressablesInitialized;
+    }
 
     void OnAddressablesInitialized(AsyncOperationHandle<IResourceLocator> initHandle)
     {
         Debug.Log("OnAddressablesInitialized!");
-        Addressables.LoadContentCatalogAsync(catalogPath).Completed += OnCatalogLoaded;
+        Addressables.LoadContentCatalogAsync(_remoteCatalogPath).Completed += OnCatalogLoaded;
         Addressables.Release(initHandle);
     }
 
     void OnCatalogLoaded(AsyncOperationHandle<IResourceLocator> catalogHandle)
     {
         Debug.Log("OnCatalogLoaded!");
-        Addressables.LoadAssetAsync<Texture2D>(addressableKeys[0]).Completed += OnTextureLoaded;
+
+        foreach (string key in addressableKeys)
+        {
+            Addressables.LoadAssetAsync<Texture2D>(key).Completed += OnTextureLoaded;
+        }
+
         Addressables.Release(catalogHandle);
     }
 
     void OnTextureLoaded(AsyncOperationHandle<Texture2D> textureHandle)
     {
-        Debug.Log("OnTextureLoaded!");
-        Texture2D texture = textureHandle.Result;
-        Sprite newSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),  Vector2.zero,  100f );
-        _image.sprite = newSprite;
+        if (textureHandle.Status == AsyncOperationStatus.Succeeded)
+        {
+            Texture2D texture = textureHandle.Result;
+            Sprite newSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 10f);
+            Image newImage = Instantiate(_templateImage, _templateImage.transform.parent);
+            newImage.sprite = newSprite;
+            newImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError($"Error al cargar textura:");
+        }
         //Addressables.Release(textureHandle);
     }
 
